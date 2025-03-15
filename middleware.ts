367cca60db08f370b850ad8d3ac6a852087e3acb
EthanEDev <ethan.ekhtiyar@gmail.com>
2025-03-15 21:39:49 +0330

    Add empty matcher configuration to middleware for NextAuth

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 // Applies to the entire project
 // export { default } from "next-auth/middleware";
+export const config = { matcher: [] };
 
 // Applies only to matching routes
 // export const config = {matcher: ["/analytics"]}
